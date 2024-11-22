b84f2a77a37d8087ca194a89e8abe1b811d36b48
VSC <veerlapatisaicharan04@gmail.com>
2024-11-22 11:16:08 +0530

    created a model for listing and added one sample listing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const Listing = require("./models/listing");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
 
 main()
@@ -18,6 +20,18 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Home route ");
+});
+
+app.get("/testing", async (req, res) => {
+  let sample = new Listing({
+    title: "place 1",
+    description: "desc about place 1",
+    price: 8654,
+    location: "hyd",
+    country: "IN",
+  });
+  await sample.save();
+  console.log("Sample was saved");
 });
 
 app.listen(8080, () => {
