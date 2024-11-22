4f02598cece03be2a130da701f01141c0c762d05
VSC <saicharanstar22@gmail.com>
2024-11-22 15:00:46 +0530

    created all listings and show views

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const path = require("path");
 const Listing = require("./models/listing");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
@@ -18,21 +18,35 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Home route ");
 });
 
-app.get("/testing", async (req, res) => {
-  let sample = new Listing({
-    title: "place 1",
-    description: "desc about place 1",
-    price: 8654,
-    location: "hyd",
-    country: "IN",
-  });
-  await sample.save();
-  console.log("Sample was saved");
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// app.get("/testing", async (req, res) => {
+//   let sample = new Listing({
+//     title: "place 1",
+//     description: "desc about place 1",
+//     price: 8654,
+//     location: "hyd",
+//     country: "IN",
+//   });
+//   await sample.save();
+//   console.log("Sample was saved");
+// });
 
 app.listen(8080, () => {
   console.log("Server is listening to port 8080");
