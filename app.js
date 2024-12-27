4c34c06e195cf6a8a23f53913993360d2a269193
iamvsc04 <saicharanstar22@gmail.com>
2024-12-27 14:06:04 +0530

    done with styling and CRUD Ops of listings

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
 
@@ -20,6 +22,11 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Home route ");
@@ -30,10 +37,42 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let id = req.params.id;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let temp = await Listing.findByIdAndDelete(id);
+  console.log(temp);
+  res.redirect("/listings");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
 });
 
 // app.get("/testing", async (req, res) => {
@@ -47,7 +86,3 @@ app.get("/listings/:id", async (req, res) => {
 //   await sample.save();
 //   console.log("Sample was saved");
 // });
-
-app.listen(8080, () => {
-  console.log("Server is listening to port 8080");
-});
